b36dd8623f5a66092b8b49160bb558d7a08d338a
AikaOnimura <aika.onimura@gmail.com>
2020-09-25 03:20:02 +0000

    [Fix]3-8 自動追記されたルートの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
