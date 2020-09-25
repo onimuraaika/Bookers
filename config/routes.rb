5c0034969f746edcf7f259d516e4ebfb8a0c03f4
AikaOnimura <aika.onimura@gmail.com>
2020-09-25 04:52:56 +0000

    [Add]3-8 コメントの追加、トップ画面のHTML追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-  # get 'homes/top'
+  root to: 'homes#top' #トップ画面をルーとパス
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
 end
