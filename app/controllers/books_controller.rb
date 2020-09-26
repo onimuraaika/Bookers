627818931baf7360bfda70410d365f2c626bbef7
AikaOnimura <aika.onimura@gmail.com>
2020-09-26 12:19:05 +0000

    [Add]3-8 投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,20 @@ class BooksController < ApplicationController
   end
 
   def new
+      @book = Book.new
+  end
+
+  def create
+      book = Book.new(book_params)
+      book.save
+      redirect_to '/books/:id' #詳細表示へ
   end
 
   def edit
   end
 end
+
+private
+  def book_params
+    params.permit(:Title, :Body) #「.require(:book)」が不要だった
+  end
