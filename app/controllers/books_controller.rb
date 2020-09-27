e85d76f9dd6af79b47bbc8866e01386c51b08b40
AikaOnimura <aika.onimura@gmail.com>
2020-09-27 07:35:05 +0000

    [Fix/Add]3-8 投稿機能・一覧表示機能の修正、削除機能・編集機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,31 @@
 class BooksController < ApplicationController
   def index
       @books = Book.all
+      @book = Book.new
   end
 
   def show
       @book = Book.find(params[:id])
   end
 
-  def new
-      @book = Book.new
-  end
-
   def create
       book = Book.new(book_params)
       book.save
-      redirect_to '/books' #詳細表示へ
+      redirect_to book_path(book) #詳細表示へ
   end
 
   def edit
+      @book = Book.find(params[:id])
   end
-end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+  end
 
 private
   def book_params
-    params.require(:Book).permit(:title, :body)
+      params.require(:book).permit(:title, :body)
   end
+end
