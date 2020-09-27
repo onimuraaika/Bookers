a4d2934ae95d34336433cca43ecf13426a496e07
AikaOnimura <aika.onimura@gmail.com>
2020-09-27 08:59:42 +0000

    [Add]3-8 リダイレクト先の修正、サクセスメッセージ表示機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,17 +11,23 @@ class BooksController < ApplicationController
   def create
       book = Book.new(book_params)
       book.save
-      redirect_to book_path(book) #詳細表示へ
+      redirect_to book_path(book), notice: 'Book was successfully created.' #詳細表示へ
   end
 
   def edit
       @book = Book.find(params[:id])
   end
 
+  def update
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.' #詳細画面へ
+  end
+
   def destroy
       book = Book.find(params[:id])
       book.destroy
-      redirect_to books_path
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
 private
